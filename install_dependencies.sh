c69fc185adf1dcc79b0de37b5bcf517bf309e1b9
Guillaume <guillaumeguar@gmail.com>
2021-04-02 16:52:48 +0200

    initial commit

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -2,7 +2,4 @@
 pip install -r requirements.txt
 apt update && apt install -y default-jdk
 apt install bc
-cd /media/bizon/DATA/guillaume/patent_database/src/
-bash install.sh
-cd /media/bizon/DATA/guillaume/bert_summary/src/
 apt install vim -y
